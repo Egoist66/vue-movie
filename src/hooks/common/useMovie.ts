c464bf35260cc31f64c3a6d3b9e8bf0ba4f7d779
Farid Mahmudov <Razormad666@gmail.com>
2024-08-06 21:37:58 +0300

    update

--- src/hooks/common/useMovie.ts
+++ src/hooks/common/useMovie.ts
@@ -1,9 +1,13 @@
 import { MoviesApi } from "@/api/movies.api"
 import { useAppStore, type SingleMovie } from "@/stores/app-store"
+import { ref } from "vue"
 
 export const useMovie = () => {
 
     const {setSingleMovie} = useAppStore()
+    const isLoading = ref<boolean>(false)
+    const isError = ref<boolean>(false)
+    
 
     const getSingleMovie = async (id: any) => {
         const data = await MoviesApi.get<SingleMovie>(`${MoviesApi.baseUrl}/?i=${id}&${MoviesApi.apiKey}`)
@@ -13,6 +17,8 @@ export const useMovie = () => {
 
 
     return {
-        getSingleMovie
+        getSingleMovie,
+        isError,
+        isLoading
     }
 }
--- src/views/FilmView.vue
+++ src/views/FilmView.vue
@@ -1,42 +1,89 @@
 <script lang="ts" setup>
 import { useMovie } from '@/hooks/common/useMovie';
 import { useAppStore } from '@/stores/app-store';
+import { delay } from '@/utils/delay';
 import { storeToRefs } from 'pinia';
 import { onBeforeMount } from 'vue';
-import {useRoute } from 'vue-router';
+import { useRoute } from 'vue-router';
 
 
-    const {getSingleMovie} = useMovie()
-    const route = useRoute()
+const { getSingleMovie, isError, isLoading } = useMovie()
+const route = useRoute()
 
-    console.log(route);
-    
-    onBeforeMount(async () => {
-        await getSingleMovie(route?.params?.id);
-    })
+console.log(route);
 
-    const {singleMovie} = storeToRefs(useAppStore())
-    
+onBeforeMount(async () => {
+  isLoading.value = true;
+  try {
+    await delay(1000);
+    await getSingleMovie(route?.params?.id);
+  }
+  catch (e) {
+    isError.value = true;
+  }
+  finally {
+    isLoading.value = false;
+  }
+})
+
+const { singleMovie } = storeToRefs(useAppStore())
+
 </script>
 <template>
-  <div class="film-view">
-    <div class="film-view-img">
-      <img :src="singleMovie?.Poster === 'N/A' ? '/placeholder.jpg' : singleMovie?.Poster" :alt="singleMovie?.Title">
-    </div>
-    <div class="film-view-info">
-      <h2>Name:{{ singleMovie?.Title }}</h2>
-      <p>Year:{{ singleMovie?.Year }}</p>
-      <p>Description:{{ singleMovie?.Plot }}</p>
-      <p>ID:{{ singleMovie?.imdbID }}</p>
-      <p>Type:{{ singleMovie?.Type }}</p>
-      <p>Genre:{{ singleMovie?.Genre }}</p>
-      <p>Rate:{{ singleMovie?.imdbRating }}</p>
+  <div :class="{ 'spinner-wrapper': isLoading }">
+    <div v-if="isLoading" class="spinner"></div>
+
+
+    <div v-else class="film-view">
+
+      <div class="film-view-img">
+        <img :src="singleMovie?.Poster === 'N/A' ? '/placeholder.jpg' : singleMovie?.Poster" :alt="singleMovie?.Title">
+      </div>
+      <div class="film-view-info">
+        <h2>Name:{{ singleMovie?.Title }}</h2>
+        <p>Year:{{ singleMovie?.Year }}</p>
+        <p>Description:{{ singleMovie?.Plot }}</p>
+        <p>ID:{{ singleMovie?.imdbID }}</p>
+        <p>Type:{{ singleMovie?.Type }}</p>
+        <p>Genre:{{ singleMovie?.Genre }}</p>
+        <p>Rate:{{ singleMovie?.imdbRating }}</p>
+      </div>
     </div>
 
   </div>
+
+
 </template>
 
 <style scoped>
+.spinner {
+  width: 56px;
+  height: 56px;
+  border-radius: 50%;
+  background: radial-gradient(farthest-side, #76acc1 94%, #0000) top/9px 9px no-repeat,
+    conic-gradient(#0000 30%, #76acc1);
+  -webkit-mask: radial-gradient(farthest-side, #0000 calc(100% - 9px), #000 0);
+  animation: spinner-c7wet2 1s infinite linear;
+}
+
+.spinner-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+@keyframes spinner-c7wet2 {
+  100% {
+    transform: rotate(1turn);
+  }
+}
+
+.loading {
+  font-size: 36px;
+  text-align: center;
+}
+
 .film-view {
   display: flex;
   flex-wrap: wrap;
@@ -53,8 +100,8 @@ import {useRoute } from 'vue-router';
 }
 
 img {
-    height: 100%;
-    border-radius: 10px;
+  height: 100%;
+  border-radius: 10px;
 }
 
 .film-view-info {
@@ -67,7 +114,8 @@ img {
   margin-top: 5px;
 }
 
-.film-view h2, p {
+.film-view h2,
+p {
   margin-bottom: 5px;
 }
 </style>
