51685927aa803de16d042313d0fb4a20881ade1b
Farid Mahmudov <Razormad666@gmail.com>
2024-08-06 22:12:59 +0300

    pagination update

--- src/components/ui/MoviePagination.vue
+++ src/components/ui/MoviePagination.vue
@@ -16,12 +16,17 @@ watchEffect(() => {
 <template>
     <div v-if="movieStore.movies.Response !== 'False'" class="movie-pagination">
         
+        <button v-if="pages > 10" :disabled="movieStore.currentPage === 1"  @click="movieStore.setCurrentPage(1)"><<</button>
         <button :disabled="movieStore.currentPage === 1"  @click="movieStore.setCurrentPage(movieStore.currentPage - 1)"><</button>
-        <div :key="item" v-for="item in pages">
-            <button @click="movieStore.setCurrentPage(item)" :class="{ 'active-page': item === movieStore.currentPage }">{{
+        <div :key="item" v-for="item in Math.min(10, pages)">
+
+            <button v-if="item >= 1 && item <= 10"  @click="movieStore.setCurrentPage(item)" :class="{ 'active-page': item === movieStore.currentPage }">{{
                 item }}</button>
         </div>
-        <button :disabled="movieStore.currentPage === pages"  @click="movieStore.setCurrentPage(movieStore.currentPage + 1)">></button>
+        <button v-if="pages > 10" :disabled="movieStore.currentPage === pages"  @click="movieStore.setCurrentPage(movieStore.currentPage + 1)">></button>
+        <button v-if="pages > 10" :disabled="movieStore.currentPage === pages"  @click="movieStore.setCurrentPage(pages)">>></button>
+
+        {{ movieStore.currentPage }} / {{ pages }}
     </div>
 </template>
 
